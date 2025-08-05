8d30a323c396132c8f4dc0f0ef30d15fd4d18d62
Bright  Boafo <bboafo003@st.ug.edu.gh>
2025-08-05 21:43:35 +0000

    Task 2: Abstract Classes and Methods

---
+++ Dcit318 New Project 2/AbstractClasses.cs
@@ -0,0 +1,54 @@
+﻿// File: AbstractClassDemo.cs
+using System;
+
+namespace Assignment2
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class AbstractClassDemo
+    {
+        public static void Run()
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+        }
+    }
+}
