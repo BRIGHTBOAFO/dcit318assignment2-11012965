176e967f0f5d1bb32bf974098a482e7bf50b465e
Bright  Boafo <bboafo003@st.ug.edu.gh>
2025-08-05 21:48:51 +0000

    Added the interface application file

---
+++ Dcit318 New Project 2/Interface.cs
@@ -0,0 +1,39 @@
+﻿// File: InterfaceDemo.cs
+using System;
+
+namespace Assignment2
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class InterfaceDemo
+    {
+        public static void Run()
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
+
