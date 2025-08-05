ec2afbc644ca09bc5cf097340dfd1d81d0e57c3e
Bright  Boafo <bboafo003@st.ug.edu.gh>
2025-08-05 21:31:17 +0000

    Add project files.

---
+++ Dcit318 New Project 2/Inheritance.cs
@@ -0,0 +1,43 @@
+﻿// File: InheritanceDemo.cs
+using System;
+
+namespace Assignment2
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class InheritanceDemo
+    {
+        public static void Run()
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
